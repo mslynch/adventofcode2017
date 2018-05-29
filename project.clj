495392378ab166f65cd54c56526f81799828e580
Matthew Lynch <mslynch95@gmail.com>
2018-05-29 00:54:41 -0500

    took things out of project.clj and moved to profiles.clj

--- project.clj
+++ project.clj
@@ -3,15 +3,7 @@
   :url "https://github.com/mslynch/adventofcode2017"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.cli "0.3.7"]
-                 [proto-repl "0.3.1"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :main ^:skip-aot adventofcode2017.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}}
-  :plugins [[lein-cljfmt "0.5.7"]
-            [lein-kibit "0.1.6"]
-            [lein-bikeshed "0.5.1"]
-            [lein-ancient "0.6.15"]
-            [jonase/eastwood "0.2.6"]
-            [venantius/yagni "0.1.4"]])
+  :profiles {:uberjar {:aot :all}})
